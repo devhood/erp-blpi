6d88e0a0c83ada7527028706d9d373c8df3bbdd2
Felicito Cascante <chito.cascante@gmail.com>
2014-10-19 01:25:39 +0800

    ang push ng tanga na si lester na wala naman palang ginawa

---
+++ module/Sales/src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Product\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+}
