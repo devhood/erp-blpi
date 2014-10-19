dfd6233b8bb40477dcb866a3585c4aaeab8f8e6b
Felicito Cascante <chito.cascante@gmail.com>
2014-10-19 11:10:28 +0800

    product zend form

--- module/Product/src/Form/AddProductForm.php
+++ module/Product/src/Form/AddProductForm.php
@@ -18,7 +18,33 @@ class AddProductForm extends Form
 						'id' => 'productCode',
 				)
 		));
-		
+
+		$this->add( array(
+				'name' => 'productName',
+				'type' => 'Text',
+				'attributes' => array(
+						'class' => 'form-control',
+						'placeholder' => 'Enter Name',
+						'id' => 'productName',
+				)
+		));
+
+		$this->add(array(
+				'name' => 'classifications[classificationId]',
+				'type' => 'Select',
+				'attributes' => array(
+						'class' => 'select2me form-control',
+						'data-placeholder' => 'Choose Classification',
+						'tabindex' => '1',
+						'id' => 'classificationId',
+				),
+				'options' => array(
+// 						'value_options' => array(
+// 								'value_options' => $this->getGatewayTable()['brandTable']->fetchSelectOption(),
+// 						),
+				)
+		));
+
 		$this->add(array(
 				'name' => 'brand[brandId]',
 				'type' => 'Select',
@@ -27,6 +53,58 @@ class AddProductForm extends Form
 						'data-placeholder' => 'Choose a Brand',
 						'tabindex' => '1',
 						'id' => 'brandId',
+				),
+				'options' => array(
+// 						'value_options' => array(
+// 								'value_options' => $this->getGatewayTable()['brandTable']->fetchSelectOption(),
+// 						),
+				)
+		));
+
+		$this->add(array(
+				'name' => 'supplier[supplierId]',
+				'type' => 'Select',
+				'attributes' => array(
+						'class' => 'select2me form-control',
+						'data-placeholder' => 'Choose Supplier',
+						'tabindex' => '1',
+						'id' => 'supplierId',
+				),
+				'options' => array(
+// 						'value_options' => array(
+// 								'value_options' => $this->getGatewayTable()['brandTable']->fetchSelectOption(),
+// 						),
+				)
+		));
+
+		$this->add( array(
+				'name' => 'size',
+				'type' => 'Text',
+				'attributes' => array(
+						'class' => 'form-control',
+						'placeholder' => 'Enter Size',
+						'id' => 'size',
+				)
+		));
+
+		$this->add( array(
+				'name' => 'weight',
+				'type' => 'Text',
+				'attributes' => array(
+						'class' => 'form-control',
+						'placeholder' => 'Enter Weight',
+						'id' => 'weight',
+				)
+		));
+
+		$this->add(array(
+				'name' => 'product_status',
+				'type' => 'Select',
+				'attributes' => array(
+						'class' => 'select2me form-control',
+						'data-placeholder' => 'Choose Status',
+						'tabindex' => '1',
+						'id' => 'product_status',
 				),
 				'options' => array(
 // 						'value_options' => array(
