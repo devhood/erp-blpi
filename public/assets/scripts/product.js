f9aac5c469d7053ab26606928c88bba4b502ad2e
facascante <facascante@smart.com.ph>
2014-10-19 08:47:04 +0800

    add select

--- public/assets/scripts/product.js
+++ public/assets/scripts/product.js
@@ -1,25 +1,10 @@
+
 var Product = function () {
 
 	return {
 
 		init: function () {
-
-			function getClassificationName(cb){
-				ajaxSelect("Classifications",{},cb);
-			};
-			$('#classificationId').select2({
-                query: function (query){
-                	getClassificationName(function(err,data){
-						var content = [];
-						for(var i = 0; i<data.data.length; i++){
-							content.push({id:data.data[i].classificationId,text:data.data[i].classificationName});
-						}
-						return query.callback(content);
-					});
-
-                    
-                }
-            });
+			
 		}
 	};
 
