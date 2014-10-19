166ba05b6e4cc13da0fc3dcf9a5b34d78aa974a6
Felicito Cascante <chito.cascante@gmail.com>
2014-10-19 15:22:07 -0700

    customer module

---
+++ module/Customer/src/Controller/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Customer\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+	protected $em = null;
+
+	public function setEntityManager(\Doctrine\ORM\EntityManager $em)
+	{
+		$this->em = $em;
+	}
+
+	public function getEntityManager()
+	{
+		if (null === $this->em) {
+			$this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+		}
+		return $this->em;
+	}
+
+    public function indexAction()
+    {
+    	return new ViewModel();
+    }
+    public function addAction()
+    {
+    	$form = new \Customer\Form\AddCustomer();
+    	return new ViewModel(array('form' => $form));
+    }
+}
