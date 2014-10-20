d403542df7fa20b1ebfaa2ab77d8f7c2e1615b1a
facascante <facascante@smart.com.ph>
2014-10-20 08:08:17 +0800

    add database for sales,inventory and shipment

--- data/blpi.sql
+++ data/blpi.sql
@@ -518,6 +518,463 @@ CREATE TABLE IF NOT EXISTS `blpi`.`Contacts` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `blpi`.`Transaction_Status`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Transaction_Status` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Transaction_Status` (
+  `transaction_status_id` INT NOT NULL AUTO_INCREMENT,
+  `transaction_status_name` VARCHAR(45) NULL,
+  `recod_status` VARCHAR(45) NULL DEFAULT 'Active',
+  PRIMARY KEY (`transaction_status_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Inventory_Locations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Inventory_Locations` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Inventory_Locations` (
+  `location_id` INT NOT NULL AUTO_INCREMENT,
+  `location_name` VARCHAR(100) NULL,
+  `address_id` INT NULL,
+  `record_status` VARCHAR(45) NULL DEFAULT 'Active',
+  PRIMARY KEY (`location_id`),
+  INDEX `fk_Inventory_Locations_Address1_idx` (`address_id` ASC),
+  CONSTRAINT `fk_Inventory_Locations_Address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `blpi`.`Address` (`address_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Transaction_Types`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Transaction_Types` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Transaction_Types` (
+  `transaction_type_id` INT NOT NULL AUTO_INCREMENT,
+  `transaction_type_name` VARCHAR(45) NULL,
+  `record_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`transaction_type_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Order_Source`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Order_Source` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Order_Source` (
+  `order_source_id` INT NOT NULL AUTO_INCREMENT,
+  `order_source_name` VARCHAR(45) NULL,
+  `record_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`order_source_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Sales`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Sales` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Sales` (
+  `sales_id` INT NOT NULL AUTO_INCREMENT,
+  `sono` VARCHAR(45) NULL,
+  `drno` VARCHAR(45) NULL,
+  `sino` VARCHAR(45) NULL,
+  `rmrno` VARCHAR(45) NULL,
+  `cmno` VARCHAR(45) NULL,
+  `pmno` VARCHAR(45) NULL,
+  `transaction_type_id` INT NULL,
+  `customer_id` INT NULL,
+  `inventory_location_id` INT NULL,
+  `shipping_address_id` INT NULL,
+  `billing_address_id` INT NULL,
+  `order_source_id` INT NULL,
+  `shipping_mode_id` INT NULL,
+  `sales_executive_id` INT NULL,
+  `payment_term_id` INT NULL,
+  `delivery_date` DATE NULL,
+  `ordered_by` VARCHAR(100) NULL,
+  `price_type_id` INT NULL,
+  `promo_id` INT NULL,
+  `percent_vat` FLOAT NULL,
+  `total_items` INT NULL,
+  `total_vat` FLOAT NULL,
+  `total_sales` FLOAT NULL,
+  `total_items_return` INT NULL,
+  `total_vat_return` FLOAT NULL,
+  `total_sales_return` FLOAT NULL,
+  `grand_total` FLOAT NULL,
+  `orno` VARCHAR(45) NULL,
+  `prno` VARCHAR(45) NULL,
+  `payment_status` VARCHAR(45) NULL,
+  `transaction_status_id` INT NULL,
+  `sales_notes` TEXT NULL,
+  PRIMARY KEY (`sales_id`),
+  INDEX `fk_Sales_Transaction_Status1_idx` (`transaction_status_id` ASC),
+  INDEX `fk_Sales_Inventory_Locations1_idx` (`inventory_location_id` ASC),
+  INDEX `fk_Sales_Address1_idx` (`shipping_address_id` ASC),
+  INDEX `fk_Sales_Address2_idx` (`billing_address_id` ASC),
+  INDEX `fk_Sales_Customers1_idx` (`customer_id` ASC),
+  INDEX `fk_Sales_Transaction_Types1_idx` (`transaction_type_id` ASC),
+  INDEX `fk_Sales_Order_Source1_idx` (`order_source_id` ASC),
+  INDEX `fk_Sales_Payment_Terms1_idx` (`payment_term_id` ASC),
+  INDEX `fk_Sales_Price_Types1_idx` (`price_type_id` ASC),
+  UNIQUE INDEX `sono_UNIQUE` (`sono` ASC),
+  UNIQUE INDEX `drno_UNIQUE` (`drno` ASC),
+  UNIQUE INDEX `sino_UNIQUE` (`sino` ASC),
+  UNIQUE INDEX `rmrno_UNIQUE` (`rmrno` ASC),
+  UNIQUE INDEX `cmno_UNIQUE` (`cmno` ASC),
+  UNIQUE INDEX `pmno_UNIQUE` (`pmno` ASC),
+  CONSTRAINT `fk_Sales_Transaction_Status1`
+    FOREIGN KEY (`transaction_status_id`)
+    REFERENCES `blpi`.`Transaction_Status` (`transaction_status_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Inventory_Locations1`
+    FOREIGN KEY (`inventory_location_id`)
+    REFERENCES `blpi`.`Inventory_Locations` (`location_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Address1`
+    FOREIGN KEY (`shipping_address_id`)
+    REFERENCES `blpi`.`Address` (`address_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Address2`
+    FOREIGN KEY (`billing_address_id`)
+    REFERENCES `blpi`.`Address` (`address_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Customers1`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `blpi`.`Customers` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Transaction_Types1`
+    FOREIGN KEY (`transaction_type_id`)
+    REFERENCES `blpi`.`Transaction_Types` (`transaction_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Order_Source1`
+    FOREIGN KEY (`order_source_id`)
+    REFERENCES `blpi`.`Order_Source` (`order_source_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Payment_Terms1`
+    FOREIGN KEY (`payment_term_id`)
+    REFERENCES `blpi`.`Payment_Terms` (`payment_term_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sales_Price_Types1`
+    FOREIGN KEY (`price_type_id`)
+    REFERENCES `blpi`.`Price_Types` (`price_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Sales_Items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Sales_Items` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Sales_Items` (
+  `sales_item_id` INT NOT NULL AUTO_INCREMENT,
+  `sales_id` INT NULL,
+  `product_id` INT NULL,
+  `quantity` INT NULL,
+  `quantity_return` INT NULL,
+  `price` FLOAT NULL,
+  `freebie` VARCHAR(45) NULL,
+  `vat` FLOAT NULL,
+  `sales` FLOAT NULL,
+  `vat_return` FLOAT NULL,
+  `sales_return` FLOAT NULL,
+  `return_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`sales_item_id`),
+  INDEX `fk_Sales_Items_Sales1_idx` (`sales_id` ASC),
+  CONSTRAINT `fk_Sales_Items_Sales1`
+    FOREIGN KEY (`sales_id`)
+    REFERENCES `blpi`.`Sales` (`sales_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Counters`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Counters` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Counters` (
+  `counter_id` INT NOT NULL AUTO_INCREMENT,
+  `counter_type` VARCHAR(45) NULL,
+  `prefix` VARCHAR(45) NULL,
+  `suffix` VARCHAR(45) NULL,
+  `count` INT ZEROFILL NULL,
+  `record_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`counter_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Sales_Payment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Sales_Payment` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Sales_Payment` (
+  `sp_id` INT NOT NULL AUTO_INCREMENT,
+  `sales_id` INT NULL,
+  `payment_type_id` INT NULL,
+  `payment_date` DATE NULL,
+  `check_no` VARCHAR(45) NULL,
+  `check_date` DATE NULL,
+  `bank` VARCHAR(100) NULL,
+  `branch` VARCHAR(45) NULL,
+  `amount` FLOAT NULL,
+  `deposit_date` DATE NULL,
+  PRIMARY KEY (`sp_id`),
+  INDEX `fk_Sales_Payment_Sales1_idx` (`sales_id` ASC),
+  CONSTRAINT `fk_Sales_Payment_Sales1`
+    FOREIGN KEY (`sales_id`)
+    REFERENCES `blpi`.`Sales` (`sales_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Product_Conditions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Product_Conditions` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Product_Conditions` (
+  `condition_id` INT NOT NULL AUTO_INCREMENT,
+  `condtion_name` VARCHAR(45) NULL,
+  `record_status` VARCHAR(45) NULL DEFAULT 'Active',
+  PRIMARY KEY (`condition_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Product_Inventories`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Product_Inventories` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Product_Inventories` (
+  `inventory_id` INT NOT NULL AUTO_INCREMENT,
+  `product_id` INT NULL,
+  `location_id` INT NULL,
+  `stock` INT NULL,
+  `max_stock_limit` INT NULL,
+  `min_stock_limit` INT NULL,
+  `reorder_limit` INT NULL,
+  `condition_id` INT NULL,
+  `cycle_count` INT NULL,
+  `last_cycle_count` DATE NULL,
+  `variance` INT NULL,
+  `cycle_count_by` INT NULL,
+  PRIMARY KEY (`inventory_id`),
+  INDEX `fk_Product_Inventories_Inventory_Locations1_idx` (`location_id` ASC),
+  INDEX `fk_Product_Inventories_Product_Conditions1_idx` (`condition_id` ASC),
+  INDEX `fk_Product_Inventories_Products1_idx` (`product_id` ASC),
+  CONSTRAINT `fk_Product_Inventories_Inventory_Locations1`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `blpi`.`Inventory_Locations` (`location_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Product_Inventories_Product_Conditions1`
+    FOREIGN KEY (`condition_id`)
+    REFERENCES `blpi`.`Product_Conditions` (`condition_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Product_Inventories_Products1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `blpi`.`Products` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Product_Movement_History`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Product_Movement_History` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Product_Movement_History` (
+  `pmh_id` INT NOT NULL AUTO_INCREMENT,
+  `inventory_id` INT NULL,
+  `stock` INT NULL,
+  `delta` INT NULL,
+  `balance` INT NULL,
+  `sales_id` INT NULL,
+  `shipment_id` INT NULL,
+  `adjustment_id` INT NULL,
+  PRIMARY KEY (`pmh_id`),
+  INDEX `fk_Product_Movement_History_Product_Inventories1_idx` (`inventory_id` ASC),
+  INDEX `fk_Product_Movement_History_Sales1_idx` (`sales_id` ASC),
+  CONSTRAINT `fk_Product_Movement_History_Product_Inventories1`
+    FOREIGN KEY (`inventory_id`)
+    REFERENCES `blpi`.`Product_Inventories` (`inventory_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Product_Movement_History_Sales1`
+    FOREIGN KEY (`sales_id`)
+    REFERENCES `blpi`.`Sales` (`sales_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Sales_History`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Sales_History` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Sales_History` (
+  `sh_id` INT NOT NULL AUTO_INCREMENT,
+  `sales_id` INT NULL,
+  `transaction_status_id` INT NULL,
+  `sales_notes` VARCHAR(45) NULL,
+  `user_id` INT NULL,
+  `transaction_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sh_id`),
+  CONSTRAINT `fk_Sales_History_Sales1`
+    FOREIGN KEY (`sales_id`)
+    REFERENCES `blpi`.`Sales` (`sales_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Purchases`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Purchases` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Purchases` (
+  `purchase_id` INT NOT NULL AUTO_INCREMENT,
+  `supplier_id` INT NULL,
+  `reference_no` VARCHAR(45) NULL,
+  `purchase_date` DATE NULL,
+  `expected_arrival_date` DATE NULL,
+  `purchase_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`purchase_id`),
+  INDEX `fk_Purchases_Suppliers1_idx` (`supplier_id` ASC),
+  CONSTRAINT `fk_Purchases_Suppliers1`
+    FOREIGN KEY (`supplier_id`)
+    REFERENCES `blpi`.`Suppliers` (`supplier_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Shipments`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Shipments` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Shipments` (
+  `shipment_id` INT NOT NULL AUTO_INCREMENT,
+  `supplier_id` INT NULL,
+  `reference_no` VARCHAR(45) NULL,
+  `arrival_date` DATE NULL,
+  `confirmed_by` INT NULL,
+  `approved_by` INT NULL,
+  `shipment_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`shipment_id`),
+  INDEX `fk_Shipments_Suppliers1_idx` (`supplier_id` ASC),
+  CONSTRAINT `fk_Shipments_Suppliers1`
+    FOREIGN KEY (`supplier_id`)
+    REFERENCES `blpi`.`Suppliers` (`supplier_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Shipment_Items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Shipment_Items` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Shipment_Items` (
+  `shipment_item_id` INT NOT NULL AUTO_INCREMENT,
+  `shipment_id` INT NULL,
+  `product_id` INT NULL,
+  `quantity` INT NULL,
+  `cost` FLOAT NULL,
+  `currency_id` INT NULL,
+  `expiry_date` VARCHAR(45) NULL,
+  `condition_id` INT NULL,
+  PRIMARY KEY (`shipment_item_id`),
+  INDEX `fk_Shipment_Items_Shipments1_idx` (`shipment_id` ASC),
+  INDEX `fk_Shipment_Items_Products1_idx` (`product_id` ASC),
+  INDEX `fk_Shipment_Items_Currencies1_idx` (`currency_id` ASC),
+  INDEX `fk_Shipment_Items_Product_Conditions1_idx` (`condition_id` ASC),
+  CONSTRAINT `fk_Shipment_Items_Shipments1`
+    FOREIGN KEY (`shipment_id`)
+    REFERENCES `blpi`.`Shipments` (`shipment_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Shipment_Items_Products1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `blpi`.`Products` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Shipment_Items_Currencies1`
+    FOREIGN KEY (`currency_id`)
+    REFERENCES `blpi`.`Currencies` (`currency_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Shipment_Items_Product_Conditions1`
+    FOREIGN KEY (`condition_id`)
+    REFERENCES `blpi`.`Product_Conditions` (`condition_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blpi`.`Purchase_Items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blpi`.`Purchase_Items` ;
+
+CREATE TABLE IF NOT EXISTS `blpi`.`Purchase_Items` (
+  `purchase_item_id` INT NOT NULL AUTO_INCREMENT,
+  `purchase_id` INT NULL,
+  `product_id` INT NULL,
+  `quantity` INT NULL,
+  `cost` FLOAT NULL,
+  `currency_id` INT NULL,
+  PRIMARY KEY (`purchase_item_id`),
+  INDEX `fk_Purchase_Items_Purchases1_idx` (`purchase_id` ASC),
+  INDEX `fk_Purchase_Items_Products1_idx` (`product_id` ASC),
+  INDEX `fk_Purchase_Items_Currencies1_idx` (`currency_id` ASC),
+  CONSTRAINT `fk_Purchase_Items_Purchases1`
+    FOREIGN KEY (`purchase_id`)
+    REFERENCES `blpi`.`Purchases` (`purchase_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Purchase_Items_Products1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `blpi`.`Products` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Purchase_Items_Currencies1`
+    FOREIGN KEY (`currency_id`)
+    REFERENCES `blpi`.`Currencies` (`currency_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
