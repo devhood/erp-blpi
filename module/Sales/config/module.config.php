bca29a739b4839de7352bab9667e80d64af0accd
Felicito Cascante <chito.cascante@gmail.com>
2014-10-22 16:39:55 -0700

    sales update

--- module/Sales/config/module.config.php
+++ module/Sales/config/module.config.php
@@ -21,9 +21,25 @@ return array(
 			),
 		),
 	),
+	'router' => array(
+	 'routes' => array(
+			__NAMESPACE__.'_Delivery_Add' => array(
+				'type' => 'Literal',
+				'options' => array(
+					'route'    => '/sales/order/add',
+					'defaults' => array(
+						'controller' => __NAMESPACE__ . '\Order',
+						'action'     => 'add',
+					),
+				),
+			),
+		),
+	),
 	'controllers' => array(
 		'invokables' => array(
 			__NAMESPACE__ . '\Delivery' => __NAMESPACE__ . '\Controller\DeliveryController',
+			__NAMESPACE__ . '\Invoice' => __NAMESPACE__ . '\Controller\InvoiceController',
+			__NAMESPACE__ . '\Order' => __NAMESPACE__ . '\Controller\OrderController',
 		),
 	),
 );
