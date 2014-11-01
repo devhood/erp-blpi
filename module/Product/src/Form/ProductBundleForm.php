e49d136ccb4ca4b12043cc2caea1f7d5f084ab8e
midnightlj <mercadolesterjay@gmail.com>
2014-11-01 15:18:14 +0000

    product modal modified

--- module/Product/src/Form/ProductBundleForm.php
+++ module/Product/src/Form/ProductBundleForm.php
@@ -14,12 +14,15 @@ class ProductBundleForm extends Form {
 				'options' => array (
 						'object_manager' => $om,
 						'target_class' => 'Database\Entity\Products',
-						'property' => 'productName' 
+						'property' => 'productName',
+						'display_empty_item' => true,
+						'empty_item_label' => ' '
 				),
 				'attributes' => array (
 						'class' => 'select2me form-control',
 						'tabindex' => '1',
-						'id' => 'childProductId' 
+						'id' => 'childProductId',
+						'required' => true 
 				) 
 		)
 		 );
