d29f7926ba6201d05a5cd3cd71a853bdcf133f49
acebesmonte <acebesmonte@gmail.com>
2014-11-01 16:01:52 +0000

    tangang ace push

--- module/Product/src/Form/ProductDocumentForm.php
+++ module/Product/src/Form/ProductDocumentForm.php
@@ -1,68 +1,66 @@
 <?php
+
 namespace Product\Form;
 
 use Zend\Form\Form;
 
-class ProductDocumentForm extends Form
-{
-    public function __construct($om = null)
-    {
-        parent::__construct('productDocument');
-
-
-  $this->add( array(
-      'name' => 'documentName',
-      'type' => 'Text',
-      'attributes' => array(
-          'class' => 'form-control',
-          'id' => 'documentName',
-      )
-      ));
-
-  $this->add( array(
-      'name' => 'documentLink',
-      'type' => 'Text',
-      'attributes' => array(
-          'class' => 'form-control',
-          'id' => 'documentLink',
-      )
-      ));
-
-  $this->add(array(
-      'name' => 'document[documentId]',
-      'type' => 'DoctrineModule\Form\Element\ObjectSelect',
-      'options' => array(
-      'object_manager' => $om,
-      'target_class'   => 'Database\Entity\DocumentTypes',
-      'property'       => 'documentName',
-      ),
-      'attributes' => array(
-      'class' => 'select2me form-control',
-      'tabindex' => '1',
-      'id' => 'documentId',
-      ),
-
-      ));
-
-  $this->add(array(
-      'name' => 'documentType[documentTypeId]',
-      'type' => 'DoctrineModule\Form\Element\ObjectSelect',
-      'options' => array(
-          'object_manager' => $om,
-          'target_class'   => 'Database\Entity\DocumentTypes',
-          'property'       => 'documentTypeName',
-      ),
-      'attributes' => array(
-          'class' => 'select2me form-control',
-          'tabindex' => '1',
-          'id' => 'documentTypeId',
-      ),
-
-  ));
-
-
-
-
-
-    }
+class ProductDocumentForm extends Form {
+	public function __construct($om = null) {
+		parent::__construct ( 'productDocument' );
+		
+		$this->add ( array (
+				'name' => 'documentName',
+				'type' => 'Text',
+				'attributes' => array (
+						'class' => 'form-control',
+						'id' => 'documentName' 
+				) 
+		) );
+		
+		$this->add ( array (
+				'name' => 'documentLink',
+				'type' => 'Text',
+				'attributes' => array (
+						'class' => 'form-control',
+						'id' => 'documentLink' 
+				) 
+		) );
+		
+		$this->add ( array (
+				'name' => 'document[documentId]',
+				'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+				'options' => array (
+						'object_manager' => $om,
+						'target_class' => 'Database\Entity\DocumentTypes',
+						'property' => 'documentName',
+						'display_empty_item' => true,
+						'empty_item_label' => ' '
+				),
+				'attributes' => array (
+						'class' => 'select2me form-control',
+						'tabindex' => '1',
+						'id' => 'documentId' 
+				) 
+		)
+		 );
+		
+		$this->add ( array (
+				'name' => 'documentType[documentTypeId]',
+				'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+				'options' => array (
+						'object_manager' => $om,
+						'target_class' => 'Database\Entity\DocumentTypes',
+						'property' => 'documentTypeName',
+						'display_empty_item' => true,
+						'empty_item_label' => ' '
+				),
+				'attributes' => array (
+						'class' => 'select2me form-control',
+						'tabindex' => '1',
+						'id' => 'documentTypeId',
+						'required' => true
+				) 
+		)
+		 );
+	}
 }
